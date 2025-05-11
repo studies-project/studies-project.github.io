01d7b3c854922f1347c53d8f93f904dd7f3efd55
Łukasz Gołębiewski <trytgol5876@gmail.com>
2025-05-11 19:18:05 +0200

    Added a proper hamburger and fixed buttons on main page

--- hamburger.js
+++ hamburger.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-  const nav = document.getElementById("myTopnav");
-  nav.classList.toggle("responsive");
-}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
